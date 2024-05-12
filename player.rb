b255489fe645bf881970a888991f612ef6da9e88
WillWSmith <will.w.smith94@gmail.com>
2024-05-12 10:09:25 -0400

    create Player class

---
+++ player.rb
@@ -0,0 +1,20 @@
+class Player
+  def initialize(name)
+    @name = name
+    @lives = 3
+    @score = 0
+  end
+
+  def name
+    @name
+  end
+
+  def lives
+    @lives
+  end
+
+  def score
+    @score
+  end
+
+end
